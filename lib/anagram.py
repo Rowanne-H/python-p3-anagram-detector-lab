ec513adb5d3d13ea7072b45a5d504e1952b06121
YUERONG HUANG <rowanne.h@hotmail.com>
2024-06-12 21:25:38 +1000

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram():
+    def __init__(self, word):
+        self.word=word
+    
+    def match(self, list=[]):
+        lists=[]
+        for w in list:
+            if sorted(self.word) == sorted(w):
+                lists.append(w)
+        return lists
